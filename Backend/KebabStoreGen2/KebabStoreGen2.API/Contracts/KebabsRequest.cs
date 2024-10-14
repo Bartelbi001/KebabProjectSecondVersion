78631342487d1a4291adaf0e392ff14e812c7576
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-14 21:55:52 +0300

    Part of contracts have added

--- Backend/KebabStoreGen2/KebabStoreGen2.API/Contracts/KebabsRequest.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.API/Contracts/KebabsRequest.cs
@@ -5,6 +5,6 @@ namespace KebabStoreGen2.API.Contracts;
 
 public record KebabsRequest(
     [Required][MaxLength(Kebab.MAX_NAME_LENGTH)] string Name,
-    [Required] string Description,
+    [Required][MaxLength(Kebab.MAX_DESCRIPTION_LENGTH)] string Description,
     [Required] decimal Price,
     IFormFile TitleImage);
