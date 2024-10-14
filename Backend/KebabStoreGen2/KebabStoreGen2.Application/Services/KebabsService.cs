06731fdfdbb5c25bb30e6c12770224dd45337a23
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-14 21:56:21 +0300

    Part of Service have added

--- Backend/KebabStoreGen2/KebabStoreGen2.Application/Services/KebabsService.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.Application/Services/KebabsService.cs
@@ -1,6 +1,27 @@
-﻿namespace KebabStoreGen2.Application.Services;
+﻿using KebabStoreGen2.API.KebabStoreGen2.Core.Models;
+using KebabStoreGen2.Core.Abstractions;
 
-public class KebabsService
+namespace KebabStoreGen2.Application.Services;
+
+public class KebabsService : IKebabService
 {
+    public Task<Guid> CreateKebab(Kebab kebab)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<Guid> DeleteKebab(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Kebab>> GetAllKebabs()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Kebab> UpdateKebab(Guid id, string name, string description, decimal price)
+    {
+        throw new NotImplementedException();
+    }
 }
