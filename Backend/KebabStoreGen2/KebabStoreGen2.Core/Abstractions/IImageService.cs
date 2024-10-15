c45a30b3f6bc9b287124e7366bba2da801b6d005
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-16 02:34:36 +0300

    Interface IImageService have added

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IImageService.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+using KebabStoreGen2.API.KebabStoreGen2.Core.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace KebabStoreGen2.Core.Abstractions;
+
+public interface IImageService
+{
+    Task<Result<Image>> CreateImage(IFormFile titleImage, string path);
+}
