1e6fe7cce6bfa7c64e4ae455ae4a802d4953611b
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-16 02:00:54 +0300

    KebabRequest have corrected(new field with path added)

--- Backend/KebabStoreGen2/KebabStoreGen2.API/Contracts/KebabsRequest.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.API/Contracts/KebabsRequest.cs
@@ -3,6 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace KebabStoreGen2.API.Contracts;
 
+//public record KebabsRequest(
+//    [Required][MaxLength(32)] string Name,
+//    [Required][MaxLength(100)] string Description,
+//    [Required][Range(0.01, decimal.MaxValue)] decimal Price,
+//    IFormFile TitleImage);
+
 public record KebabsRequest(
     [Required][MaxLength(Kebab.MAX_NAME_LENGTH)] string Name,
     [Required][MaxLength(Kebab.MAX_DESCRIPTION_LENGTH)] string Description,
