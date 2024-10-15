1d42cb61b0c01e024d7208e54f2d089fabd3bb85
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-16 02:59:02 +0300

    Migrations have added

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.DataAccess/Migrations/20241015233725_initialgen2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KebabStoreGen2.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialgen2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KebabEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TitleImagePath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KebabEntities", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KebabEntities");
+        }
+    }
+}
