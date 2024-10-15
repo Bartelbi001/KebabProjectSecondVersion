85e28bc114dd6d3e1fa15b1d36942c12a1cc8bc8
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-16 01:38:43 +0300

    Edit IKebabService(added new argument) && IKebabRepository(added new argument)

--- Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IKebabsRepository.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IKebabsRepository.cs
@@ -5,7 +5,15 @@ namespace KebabStoreGen2.Core.Abstractions;
 public interface IKebabsRepository
 {
     Task<Guid> Create(Kebab kebab);
-    Task<Guid> Update(Guid id, string name, string description, decimal price);
     Task<Guid> Delete(Guid id);
     Task<List<Kebab>> Get();
+    Task<Guid> Update(Guid id, string name, string description, decimal price, string? titleImagePath = null); // Обновление интерфейса
 }
+
+//public interface IKebabsRepository
+//{
+//    Task<Guid> Create(Kebab kebab);
+//    Task<Guid> Update(Guid id, string name, string description, decimal price);
+//    Task<Guid> Delete(Guid id);
+//    Task<List<Kebab>> Get();
+//}
