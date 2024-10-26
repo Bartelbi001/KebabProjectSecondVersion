549be1d0d2f8086457ed306b2cd0deceb2ee4dc2
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-26 13:35:29 +0300

    Update .gitignore && deleted try-catch constructions in Program.cs

--- Backend/KebabStoreGen2/KebabStoreGen2.API/Program.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.API/Program.cs
@@ -1,5 +1,4 @@
 using KebabStoreGen2.API.Extensions;
-using Serilog;
 
 namespace KebabStoreGen2.API
 {
@@ -7,32 +6,18 @@ namespace KebabStoreGen2.API
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                Log.Information("Starting the application");
+            var builder = WebApplication.CreateBuilder(args);
 
-                var builder = WebApplication.CreateBuilder(args);
+            builder.Services.ConfigureServices(builder.Configuration);
+            builder.Services.AddSerilogServices();
+            builder.Services.AddValidatorServices();
+            builder.Services.AddSwaggerServices();
+            builder.Services.AddAuthenticationServices(builder.Configuration); // «аготовка на Gen3, будет разделен на 2 сервиса
 
-                builder.Services.ConfigureServices(builder.Configuration);
-                builder.Services.AddSerilogServices();
-                builder.Services.AddValidatorServices();
-                builder.Services.AddSwaggerServices();
-                builder.Services.AddAuthenticationServices(builder.Configuration); // «аготовка на Gen3, будет разделен на 2 сервиса
+            var app = builder.Build();
+            app.ConfigureMiddleware();
 
-                var app = builder.Build();
-                app.ConfigureMiddleware();
-
-                Log.Information("Application configured successfully");
-                app.Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            app.Run();
         }
     }
 }
