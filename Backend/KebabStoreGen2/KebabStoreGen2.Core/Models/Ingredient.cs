960bd6fefd648753244372b14f48430b4fdc7ec2
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-29 15:47:06 +0300

    Class about ingredient field of Kebab has added

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Models/Ingredient.cs
@@ -0,0 +1,62 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace KebabStoreGen2.Core.Models;
+
+public class Ingredient
+{
+    public const int MAX_INGREDIENTNAME_LENGTH = 16;
+
+    public Ingredient(Guid id, string ingredientName, int weightInGrams, int calories, int protein, int fat, int carbs)
+    {
+        Id = id;
+        IngredientName = ingredientName;
+        WeightInGrams = weightInGrams;
+        Calories = calories;
+        Protein = protein;
+        Fat = fat;
+        Carbs = carbs;
+    }
+
+    public Guid Id { get; }
+    public string IngredientName { get; private set; } = string.Empty;
+    public int WeightInGrams { get; private set; }
+    public int Calories { get; private set; }
+    public int Protein { get; private set; }
+    public int Fat { get; private set; }
+    public int Carbs { get; private set; }
+
+    public static Result<Ingredient> Create(Guid id, string ingredientName, int weightInGrams, int calories, int protein, int fat, int carbs)
+    {
+        if (string.IsNullOrWhiteSpace(ingredientName) || ingredientName.Length > MAX_INGREDIENTNAME_LENGTH)
+        {
+            return Result.Failure<Ingredient>($"'{nameof(ingredientName)}' is required and must be shorter or equal than {MAX_INGREDIENTNAME_LENGTH} characters");
+        }
+
+        if (weightInGrams < 0)
+        {
+            return Result.Failure<Ingredient>($"'{nameof(weightInGrams)}' can't be negative");
+        }
+
+        if (calories < 0)
+        {
+            return Result.Failure<Ingredient>($"'{nameof(calories)}' can't be negative");
+        }
+
+        if (protein < 0)
+        {
+            return Result.Failure<Ingredient>($"'{nameof(protein)}' can't be negative");
+        }
+
+        if (fat < 0)
+        {
+            return Result.Failure<Ingredient>($"'{nameof(fat)}' can't be negative");
+        }
+
+        if (carbs < 0)
+        {
+            return Result.Failure<Ingredient>($"'{nameof(carbs)}' can't be negative");
+        }
+
+        return Result.Success(new Ingredient(id, ingredientName, weightInGrams, calories, protein, fat, carbs));
+    }
+}
