11f3360b63c0699d003a9f301820027483fa7b31
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-10-29 15:46:00 +0300

    Interface && Serviceabout calculating calories added

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.Application/Services/CalculateTotalCaloriesService.cs
@@ -0,0 +1,18 @@
+﻿using KebabStoreGen2.Core.Abstractions;
+using KebabStoreGen2.Core.Models;
+
+namespace KebabStoreGen2.Application.Services;
+
+public class CalculateTotalCaloriesService : INutritionCalculatorService
+{
+    public int CalculateTotalCalories(List<Ingredient> ingredients)
+    {
+        int totalCalories = 0;
+        foreach (var ingredient in ingredients)
+        {
+            totalCalories += ingredient.Calories;
+        }
+
+        return totalCalories;
+    }
+}
---
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/INutritionCalculatorService.cs
@@ -0,0 +1,8 @@
+﻿using KebabStoreGen2.Core.Models;
+
+namespace KebabStoreGen2.Core.Abstractions;
+
+public interface INutritionCalculatorService
+{
+    int CalculateTotalCalories(List<Ingredient> ingredients);
+}
