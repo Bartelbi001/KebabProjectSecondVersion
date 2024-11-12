328083f436a1f110666db2324468473120f01489
Ilya_Shelkunov <shelkunov.20014@gmail.com>
2024-11-12 18:24:39 +0300

    Rework with Ingredients fields

--- Backend/KebabStoreGen2/KebabStoreGen2.Core/Models/Ingredient.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Models/Ingredient.cs
@@ -5,57 +5,41 @@ namespace KebabStoreGen2.Core.Models;
 
 public class Ingredient
 {
-    private Ingredient(Guid id, string ingredientName, int weightInGrams, int calories, int protein, int fat, int carbs)
+    private Ingredient(Guid id, string ingredientName, decimal caloriesPer100g, decimal proteinPer100g,
+        decimal fatPer100g, decimal carbsPer100g, decimal sugarPer100g, bool containsLactose)
     {
         Id = id;
         IngredientName = ingredientName;
-        WeightInGrams = weightInGrams;
-        Calories = calories;
-        Protein = protein;
-        Fat = fat;
-        Carbs = carbs;
+        CaloriesPer100g = caloriesPer100g;
+        ProteinPer100g = proteinPer100g;
+        FatPer100g = fatPer100g;
+        CarbsPer100g = carbsPer100g;
+        SugarPer100g = sugarPer100g;
+        ContainsLactose = containsLactose;
     }
 
     public Guid Id { get; }
     public string IngredientName { get; private set; } = string.Empty;
-    public int WeightInGrams { get; private set; }
-    public int Calories { get; private set; }
-    public int Protein { get; private set; }
-    public int Fat { get; private set; }
-    public int Carbs { get; private set; }
+    public decimal CaloriesPer100g { get; private set; }
+    public decimal ProteinPer100g { get; private set; }
+    public decimal FatPer100g { get; private set; }
+    public decimal CarbsPer100g { get; private set; }
+    public decimal SugarPer100g { get; private set; }
+    public bool ContainsLactose { get; private set; }
 
-    public static Result<Ingredient> Create(Guid id, string ingredientName, int weightInGrams, int calories, int protein, int fat, int carbs)
+    public static Result<Ingredient> Create(Guid id, string ingredientName, decimal caloriesPer100g, decimal proteinPer100g, decimal fatPer100g,
+        decimal carbsPer100g, decimal sugarPer100g, bool containsLactose)
     {
         if (string.IsNullOrWhiteSpace(ingredientName) || ingredientName.Length > IngredientConstants.MAX_INGREDIENTNAME_LENGTH)
         {
             return Result.Failure<Ingredient>($"'{nameof(ingredientName)}' is required and must be shorter or equal than {IngredientConstants.MAX_INGREDIENTNAME_LENGTH} characters");
         }
 
-        if (weightInGrams < 0)
+        if (caloriesPer100g < 0 || proteinPer100g < 0 || fatPer100g < 0 || carbsPer100g < 0 || sugarPer100g < 0)
         {
-            return Result.Failure<Ingredient>($"'{nameof(weightInGrams)}' can't be negative");
+            return Result.Failure<Ingredient>($"Nutritional values can't be negative");
         }
 
-        if (calories < 0)
-        {
-            return Result.Failure<Ingredient>($"'{nameof(calories)}' can't be negative");
-        }
-
-        if (protein < 0)
-        {
-            return Result.Failure<Ingredient>($"'{nameof(protein)}' can't be negative");
-        }
-
-        if (fat < 0)
-        {
-            return Result.Failure<Ingredient>($"'{nameof(fat)}' can't be negative");
-        }
-
-        if (carbs < 0)
-        {
-            return Result.Failure<Ingredient>($"'{nameof(carbs)}' can't be negative");
-        }
-
-        return Result.Success(new Ingredient(id, ingredientName, weightInGrams, calories, protein, fat, carbs));
+        return Result.Success(new Ingredient(id, ingredientName, caloriesPer100g, proteinPer100g, fatPer100g, carbsPer100g, sugarPer100g, containsLactose));
     }
 }
