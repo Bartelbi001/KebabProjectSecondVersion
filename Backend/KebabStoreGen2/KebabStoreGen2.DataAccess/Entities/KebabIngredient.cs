32764fc0c71cc48fc856fe1baddaba75d5b8e20d
Ilya Shelkunov <shelkunov.20014@gmail.com>
2024-12-19 20:46:49 +0300

    A new class whish making a dependence between IngredientEntity && KebabEntity has added

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.DataAccess/Entities/KebabIngredient.cs
@@ -0,0 +1,13 @@
+﻿namespace KebabStoreGen2.DataAccess.Entities
+{
+    public class KebabIngredient
+    {
+        public Guid KebabId { get; set; }
+        public KebabEntity Kebab { get; set; } = null!;
+
+        public Guid IngredientId { get; set; }
+        public IngredientEntity Ingredient { get; set; } = null!;
+
+        public int Weight { get; set; } // Вус конкретного ингредиента в данном кебабе
+    }
+}
