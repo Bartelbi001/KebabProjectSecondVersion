b715dc4f84b1d1cd32ec451e9ccbd84c2f657786
Ilya Shelkunov <shelkunov.20014@gmail.com>
2024-12-25 17:42:08 +0300

    Use a KebabRequest && KebabResponse in IKebabServise instead of a full count of fields right now.

--- Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IIngredientService.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IIngredientService.cs
@@ -1,12 +1,13 @@
-﻿using KebabStoreGen2.Core.Models;
+﻿using KebabStoreGen2.Core.Contracts;
+using KebabStoreGen2.Core.Models;
 
 namespace KebabStoreGen2.Core.Abstractions;
 
 public interface IIngredientService
 {
-    Task<Guid> CreateIngredient(Ingredient ingredient);
-    Task<Ingredient> GetIngredientById(Guid id);
+    Task<Guid> CreateIngredient(IngredientRequest ingredientRequest);
+    Task<IngredientResponse> GetIngredientById(Guid id);
     Task<List<Ingredient>> GetAllIngredients();
-    Task UpdateIngredient(Guid id, string ingredientName, decimal caloriesPer100g, decimal proteinPer100g, decimal fatPer100g, decimal carbsPer100g, decimal? sugarPer100g, bool? containsLactose);
+    Task UpdateIngredient(Guid id, IngredientRequest ingredientRequest);
     Task<Guid> DeleteIngredient(Guid id);
 }
--- Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IKebabService.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IKebabService.cs
@@ -1,18 +1,12 @@
 ﻿using KebabStoreGen2.Core.Contracts;
-using KebabStoreGen2.Core.Models;
-using KebabStoreGen2.Core.Models.Enums;
 
 namespace KebabStoreGen2.Core.Abstractions;
 
 public interface IKebabService
 {
-    Task<Guid> CreateKebab(string kebabName, string kebabDescription, decimal kebabPrice, StuffingCategory stuffing, WrapCategory wrap,
-        bool isAvailable, string? fileName, string? imagePath, List<Guid> existingIngredientIds,
-        List<IngredientRequest> newIngredients);
-    Task<Kebab> GetKebabById(Guid id);
-    Task<List<Kebab>> GetAllKebabs();
-    Task UpdateKebab(Guid id, string kebabName, string kebabDescription, decimal kebabPrice, StuffingCategory stuffing,
-        WrapCategory wrap, bool isAvailable, string? fileName, string? imagePath,
-        List<Guid> existingIngredientIds, List<IngredientRequest> newIngredients);
+    Task<Guid> CreateKebab(KebabRequest kebabRequest);
+    Task<KebabResponse> GetKebabById(Guid id);
+    Task<List<KebabResponse>> GetAllKebabs();
+    Task UpdateKebab(Guid id, KebabRequest kebabRequest);
     Task<Guid> DeleteKebab(Guid id);
 }
