35794af86c4dda6a1807052adfc9477ac63e38cf
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-03 15:49:43 +0300

    New Util class with Auth Google Drive have been added

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Utils/GoogleDriveAuthUtil.cs.cs
@@ -0,0 +1,18 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+
+namespace KebabStoreGen2.Core.Utils;
+
+public class GoogleDriveAuthUtil
+{
+    public static UserCredential GetGoogleCredential(string credentialsFilePath, string tokenFilePath, string[] scopes)
+    {
+        using var stream = new FileStream(credentialsFilePath, FileMode.Open, FileAccess.Read);
+        return GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.Load(stream).Secrets,
+            scopes,
+            "user",
+            CancellationToken.None,
+            new FileDataStore(tokenFilePath, true)).Result;
+    }
+}
