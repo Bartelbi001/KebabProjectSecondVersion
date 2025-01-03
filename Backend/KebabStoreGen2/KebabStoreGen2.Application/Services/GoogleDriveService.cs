7964f2d4732e6678e775e972c5abda89c883e65d
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-03 15:51:25 +0300

    New class and its interface with Auth Google Drive have been added

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.Application/Services/GoogleDriveService.cs
@@ -0,0 +1,69 @@
+﻿using CSharpFunctionalExtensions;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using KebabStoreGen2.Core.Abstractions;
+using KebabStoreGen2.Core.Models;
+using KebabStoreGen2.Core.Utils;
+using Microsoft.AspNetCore.Http;
+
+namespace KebabStoreGen2.Application.Services;
+
+public class GoogleDriveService : IGoogleDriveService
+{
+    static string[] Scopes = { DriveService.Scope.DriveFile };
+    static string ApplicationName = "KebabProjectGen2";
+    private readonly DriveService _driveService;
+
+    public GoogleDriveService()
+    {
+        var credentialsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Credentials", "credentials.json");
+        var tokenPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "token.json");
+        var credential = GoogleDriveAuthUtil.GetGoogleCredential(credentialsPath, tokenPath, Scopes);
+
+        _driveService = new DriveService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = ApplicationName,
+        });
+    }
+
+    public async Task<Result<Image>> CreateImage(IFormFile file, string fileName)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return Result.Failure<Image>("Invalid file");
+        }
+
+        var uniqueFileName = $"{Guid.NewGuid()}_{fileName}";
+        var fileMetadata = new Google.Apis.Drive.v3.Data.File
+        {
+            Name = uniqueFileName,
+            MimeType = file.ContentType,
+        };
+
+        using var stream = new MemoryStream();
+        await file.CopyToAsync(stream);
+        stream.Position = 0;
+
+        var request = _driveService.Files.Create(fileMetadata, stream, file.ContentType);
+        request.Fields = "id";
+        var response = await request.UploadAsync();
+
+        if (response.Status != Google.Apis.Upload.UploadStatus.Completed)
+        {
+            return Result.Failure<Image>("Error uploading file to Google Drive");
+        }
+
+        var fileId = request.ResponseBody.Id;
+        var imageUrl = $"https://drive.google.com/uc?id={fileId}";
+
+        var result = Image.Create(uniqueFileName, imageUrl);
+
+        if (result.IsFailure)
+        {
+            return Result.Failure<Image>(result.Error);
+        }
+
+        return Result.Success(result.Value);
+    }
+}
---
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IGoogleDriveService.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+using KebabStoreGen2.Core.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace KebabStoreGen2.Core.Abstractions;
+
+public interface IGoogleDriveService
+{
+    Task<Result<Image>> CreateImage(IFormFile file, string fileName);
+}
