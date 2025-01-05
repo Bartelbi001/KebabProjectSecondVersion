aeb990318532b57a7a761d00bb087acc108792f2
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-05 16:33:16 +0300

    feat: required status have been added to some fields

--- Backend/KebabStoreGen2/KebabStoreGen2.Core/Contracts/KebabIngredientResponse.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Contracts/KebabIngredientResponse.cs
@@ -3,12 +3,12 @@
 public class KebabIngredientResponse
 {
     public Guid Id { get; set; }
-    public string IngredientName { get; set; } = string.Empty;
-    public int Weight { get; set; }
-    public decimal CaloriesPer100g { get; set; }
-    public decimal ProteinPer100g { get; set; }
-    public decimal FatPer100g { get; set; }
-    public decimal CarbsPer100g { get; set; }
+    public required string IngredientName { get; set; } = string.Empty;
+    public required int Weight { get; set; }
+    public required decimal CaloriesPer100g { get; set; }
+    public required decimal ProteinPer100g { get; set; }
+    public required decimal FatPer100g { get; set; }
+    public required decimal CarbsPer100g { get; set; }
     public decimal? SugarPer100g { get; set; }
     public bool? ContainsLactose { get; set; }
 }
