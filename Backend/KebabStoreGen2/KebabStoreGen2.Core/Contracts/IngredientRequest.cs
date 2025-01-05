5e6ef63f9e1d4d43edb3639c603a59d73ff2e8d9
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-05 16:25:48 +0300

    Record to Class transformation for SOLID

--- Backend/KebabStoreGen2/KebabStoreGen2.Core/Contracts/IngredientRequest.cs
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Contracts/IngredientRequest.cs
@@ -3,12 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace KebabStoreGen2.Core.Contracts;
 
-public record IngredientRequest(
-    [Required][MaxLength(IngredientConstants.MAX_INGREDIENTNAME_LENGTH)] string IngredientName,
-    [Required] decimal CaloriesPer100g,
-    [Required] decimal ProteinPer100g,
-    [Required] decimal FatPer100g,
-    [Required] decimal CarbsPer100g,
-    decimal? SugarPer100g = null,
-    bool? ContainsLactose = null
-);
+public class IngredientRequest
+{
+    [MaxLength(IngredientConstants.MAX_INGREDIENTNAME_LENGTH)]
+    public required string IngredientName { get; set; }
+    public required decimal CaloriesPer100g { get; set; }
+    public required decimal ProteinPer100g { get; set; }
+    public required decimal FatPer100g { get; set; }
+    public required decimal CarbsPer100g { get; set; }
+    public decimal? SugarPer100g { get; set; }
+    public bool? ContainsLactose { get; set; }
+}
