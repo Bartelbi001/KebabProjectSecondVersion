a913edac3a657bf9df5e15f8adae609fd3f007e6
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-08 22:41:58 +0300

    refactor: a new directory for KebabIngredient

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.DataAccess/LinkingModels/KebabIngredient.cs
@@ -0,0 +1,14 @@
+﻿using KebabStoreGen2.DataAccess.Entities;
+
+namespace KebabStoreGen2.Core.Models.LinkingModels;
+
+public class KebabIngredient
+{
+    public Guid KebabId { get; set; }
+    public KebabEntity Kebab { get; set; } = null!;
+
+    public Guid IngredientId { get; set; }
+    public IngredientEntity Ingredient { get; set; } = null!;
+
+    public int Weight { get; set; } // Вес конкретного ингредиента в данном кебабе
+}
