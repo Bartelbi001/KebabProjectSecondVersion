b3d1ae12c8cd6abdeb9c0d2cda86e983037a004c
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-01-08 22:40:00 +0300

    feat: interface IMapper has been added

---
+++ Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IMapper.cs
@@ -0,0 +1,6 @@
+﻿namespace KebabStoreGen2.Core.Abstractions;
+
+public interface IMapper<TSource, TDestination>
+{
+    TDestination Map(TSource source);
+}
--- Backend/KebabStoreGen2/KebabStoreGen2.Core/Abstractions/IMappingService.cs
+++
@@ -1,8 +0,0 @@
-﻿using KebabStoreGen2.Core.Models;
-
-namespace KebabStoreGen2.Core.Abstractions;
-
-public interface IMappingService
-{
-    KebabEntity MapToKebabEntity(Kebab kebab);
-}
